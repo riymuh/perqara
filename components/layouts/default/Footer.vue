8a59b8dd5509aa05da718c5668664c73850075d8
riymuh <mriyadh103@gmail.com>
2022-12-22 11:53:05 +0700

    improvement, home, index movie, footer

--- components/layouts/default/Footer.vue
+++ components/layouts/default/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-gray-900">
+  <div class="bg-black">
     <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 text-slate-500">
       <div class="grid grid-cols-3 gap-4 justify-between py-12">
         <div class="text-left">© 2021 MoovieTime. All rights reserved.</div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,60 +1,27 @@
 <template>
-  <div class="bg-gray-800 pb-12">
+  <div class="bg-gray-900 pb-12">
     <!-- corousel banner -->
     <div class="carousel mb-20">
       <div class="carousel-inner">
-        <input
-          class="carousel-open"
-          type="radio"
-          id="carousel-1"
-          name="carousel"
-          aria-hidden="true"
-          hidden=""
-          checked="checked"
-        />
+        <input class="carousel-open" type="radio" id="carousel-1" name="carousel" aria-hidden="true" hidden=""
+          checked="checked" />
         <div class="carousel-item">
           <img src="http://fakeimg.pl/2000x800/0079D8/fff/?text=Without" />
         </div>
-        <input
-          class="carousel-open"
-          type="radio"
-          id="carousel-2"
-          name="carousel"
-          aria-hidden="true"
-          hidden=""
-        />
+        <input class="carousel-open" type="radio" id="carousel-2" name="carousel" aria-hidden="true" hidden="" />
         <div class="carousel-item">
           <img src="http://fakeimg.pl/2000x800/DA5930/fff/?text=JavaScript" />
         </div>
-        <input
-          class="carousel-open"
-          type="radio"
-          id="carousel-3"
-          name="carousel"
-          aria-hidden="true"
-          hidden=""
-        />
+        <input class="carousel-open" type="radio" id="carousel-3" name="carousel" aria-hidden="true" hidden="" />
         <div class="carousel-item">
           <img src="http://fakeimg.pl/2000x800/F90/fff/?text=Carousel" />
         </div>
-        <label for="carousel-3" class="carousel-control prev control-1"
-          >‹</label
-        >
-        <label for="carousel-2" class="carousel-control next control-1"
-          >›</label
-        >
-        <label for="carousel-1" class="carousel-control prev control-2"
-          >‹</label
-        >
-        <label for="carousel-3" class="carousel-control next control-2"
-          >›</label
-        >
-        <label for="carousel-2" class="carousel-control prev control-3"
-          >‹</label
-        >
-        <label for="carousel-1" class="carousel-control next control-3"
-          >›</label
-        >
+        <label for="carousel-3" class="carousel-control prev control-1">‹</label>
+        <label for="carousel-2" class="carousel-control next control-1">›</label>
+        <label for="carousel-1" class="carousel-control prev control-2">‹</label>
+        <label for="carousel-3" class="carousel-control next control-2">›</label>
+        <label for="carousel-2" class="carousel-control prev control-3">‹</label>
+        <label for="carousel-1" class="carousel-control next control-3">›</label>
         <ol class="carousel-indicators">
           <li>
             <label for="carousel-1" class="carousel-bullet">•</label>
@@ -75,9 +42,7 @@
           <h1 class="text-white text-2xl">Discover Movies</h1>
         </div>
         <div class="mb-12 pt-4">
-          <button
-            type="button"
-            class="
+          <button type="button" class="
               text-white
               bg-red-700
               hover:bg-red-800
@@ -91,13 +56,10 @@
               mr-2
               mb-2
               dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900
-            "
-          >
+            ">
             Popularity
           </button>
-          <button
-            type="button"
-            class="
+          <button type="button" class="
               text-white
               bg-gray-700
               hover:bg-gray-800
@@ -111,39 +73,30 @@
               mr-2
               mb-2
               dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-900
-            "
-          >
+            ">
             Release Date
           </button>
         </div>
       </div>
-      <div
-        class="
+      <div class="
           relative
           grid grid-cols-5
           gap-5
           items-center
           justify-between
-        "
-      >
+        ">
         <div class="max-w-sm relative" v-for="n in 10" :key="n">
           <a href="#">
-            <img
-              class=""
-              src="https://upload.wikimedia.org/wikipedia/id/4/46/Deadpool_poster.jpg"
-              alt=""
-            />
+            <img class="" src="https://upload.wikimedia.org/wikipedia/id/4/46/Deadpool_poster.jpg" alt="" />
           </a>
           <div class="py-2">
             <a href="#">
-              <h5
-                class="
+              <h5 class="
                   text-2l
                   font-bold
                   tracking-tight
                   text-white
-                "
-              >
+                ">
                 Deadpool
               </h5>
               <span class="text-2l font-thin text-gray-500">2016</span>
@@ -152,8 +105,7 @@
           <div class="bg-opacity-90 bg-gray-500 w-12 h-6 absolute top-0 right-0 text-white text-center">
             7.8
           </div>
-          <div
-            class="
+          <div class="
               hidden
               focus:block
               bg-black
@@ -161,8 +113,7 @@
               h-full
               absolute
               top-0
-            "
-          ></div>
+            "></div>
         </div>
       </div>
     </div>
